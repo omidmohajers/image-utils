18abc5c3a237b3de77266eb56603a700a3e39381
omidmohajers <120931404+omidmohajers@users.noreply.github.com>
2023-01-11 20:18:39 +0330

    Add files via upload
    Over the years of working with .NET and images in programs, I have collected a collection of useful functions for working with images in the form of a library.

---
+++ BitmapToBitmapSourceConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace PA.PersianUtils.ImageUtils
+{
+    public class BitmapToBitmapSourceConverter : IValueConverter
+    {
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            System.Drawing.Bitmap source = value as System.Drawing.Bitmap;
+            if (source == null || targetType != typeof(ImageSource))
+                return null;
+            return ImageUtils.Imaging.ConvertBitmapToBitmapImage(source);
+
+
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+        #endregion
+    }
+}
---
+++ DrawingColorToMediaColorConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace PA.PersianUtils.ImageUtils
+{
+    public class DrawingColorToMediaColorConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return System.Windows.Media.Color.FromArgb(255, 255, 255, 255);
+            System.Drawing.Color color = (System.Drawing.Color)value;
+            System.Windows.Media.Color result = System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B);
+            return result;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return System.Windows.Media.Color.FromArgb(255, 255, 255, 255);
+            System.Windows.Media.Color color = (System.Windows.Media.Color)value;
+            return System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B);
+        }
+    }
+
+    public class ArgbColorToMediaColorConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return System.Windows.Media.Color.FromArgb(255, 255, 255, 255);
+            System.Drawing.Color color = System.Drawing.Color.FromArgb((int)value);
+            System.Windows.Media.Color result = System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B);
+            return result;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return System.Windows.Media.Color.FromArgb(255, 255, 255, 255);
+            System.Windows.Media.Color color = (System.Windows.Media.Color)value;
+            return System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B).ToArgb();
+        }
+    }
+}
